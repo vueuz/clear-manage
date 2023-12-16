69bcb3da509f3ed64597ab81f9451dd6b61ba39b
Y.V. <yung.venue@gmail.com>
2023-12-17 00:26:30 +0800

    chore: Create app & Add package

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router'
+import ArcoVue from '@arco-design/web-vue';
+import '@arco-design/web-vue/dist/arco.css';
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+app.use(ArcoVue);
+
+app.use(router)
+
+
+app.mount('#app')
