e890cb742e0f64786db632ef952ef275b98b3d32
Y.V. <yung.venue@gmail.com>
2023-12-17 12:35:47 +0800

    init(router): Add base router

---
+++ src/router/index.ts
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import { NOT_FOUND, REDIRECT } from './routes/base'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: "login"
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/login/index.vue')
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: () => import('@/views/home/index.vue')
+    },
+    NOT_FOUND,
+    REDIRECT
+  ]
+})
+
+export default router
