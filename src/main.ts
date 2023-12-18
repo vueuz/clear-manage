786f713b29adf321ad6ecb487c701a1941051b28
Y.V. <yung.venue@gmail.com>
2023-12-19 00:12:20 +0800

    style(icon): Add icon font

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@ import App from './App.vue'
 import router from './router'
 import ArcoVue from '@arco-design/web-vue';
 import '@arco-design/web-vue/dist/arco.css';
+import { Message } from '@arco-design/web-vue';
+import '@/assets/icon/index.css'
 
 
 const app = createApp(App)
+Message._context = app._context;
 app.use(ArcoVue);
 
 app.use(router)
